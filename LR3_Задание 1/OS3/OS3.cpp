5f912b36001a957ecac29e36ff6677dfdd0531b0
Adil <81522631+IceFroggit@users.noreply.github.com>
2022-12-28 00:04:43 +0300

    Add files via upload

---
+++ LR3_Задание 1/OS3/OS3.cpp
@@ -0,0 +1,71 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const unsigned cntThread = 16; // кол-во потоков
+const unsigned block = 100; // размер блока
+const unsigned Precision = 100000000; // точность вычислений
+double pi = 0;
+
+struct Param // структура параметров
+{
+	double* pi;
+	HANDLE* mutex;
+	int* startBlock;
+};
+
+DWORD WINAPI THREAD(LPVOID lpParam) { // поток 
+	Param*  param = (Param*)lpParam; 
+	double* pi = (double*)param->pi;
+	HANDLE mutex = *(HANDLE*)param->mutex; // mutex не дает использовать одну память одновременно
+	int startBlock = *(int*)param->startBlock;
+
+	double accumulator = 0;
+
+	while (startBlock < Precision) {
+		for (int i = startBlock; i < startBlock + block; ++i) {
+			accumulator += 4 / (1 + ((i + 0.5) / Precision) * ((i + 0.5) / Precision));
+		}
+		startBlock += cntThread * block;
+	}
+
+	WaitForSingleObject(mutex, INFINITE);
+	*pi += accumulator / Precision;
+	ReleaseMutex(mutex);
+
+	return 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int start, end;
+	HANDLE Threads[cntThread], mutex;
+	int startingOperations[cntThread];
+	mutex = CreateMutex(NULL, FALSE, NULL);
+	for (int i = 0; i < cntThread; ++i) { // создание потоков
+		startingOperations[i] = i * block;
+		Param * param = new Param{ &pi, &mutex, startingOperations + i};
+		Threads[i] = CreateThread(NULL, 0, THREAD, (void*)param, CREATE_SUSPENDED, NULL);
+	}
+
+	start = GetTickCount();
+	for (int i = 0; i < cntThread; ++i){// запуск потоков
+		ResumeThread(Threads[i]);
+	} 
+
+	WaitForMultipleObjects(cntThread, Threads, TRUE, INFINITE); // ожидание завершения потоков
+	end = GetTickCount();
+
+	std::cout << "Кол-во Потоков: " << cntThread;
+	std::cout << "\nЧисло Pi = " << fixed << setprecision(20) << pi;
+	std::cout << "\nВремя : " << end - start << " мс";
+	for (int i = 0; i < cntThread; ++i){CloseHandle(Threads[i]);}
+	CloseHandle(mutex);
+	return 0;
+}
+
+
---
+++ LR3_Задание 2/OS3.2/OS3.2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <omp.h.>
+#include <windows.h>
+#include <iomanip>
+
+using namespace std;
+
+const unsigned cntThread = 1; //кол-во потоков
+const unsigned BlockSize = 100; // размер блока
+const unsigned Precision = 100000000; // точность вычислений
+double pi = 0;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int cntBlock = Precision / BlockSize;
+	int startTime, endTime;
+	omp_lock_t lock;
+	omp_set_num_threads(cntThread);
+	omp_init_lock(&lock);
+
+	startTime = GetTickCount(); 
+	#pragma omp parallel  
+	{
+		double accumulator = 0;
+
+		#pragma omp for
+		for (int block = 0; block < cntBlock; ++block)
+			for (int i = block * BlockSize; i < block * BlockSize + BlockSize; ++i)
+				accumulator += 4 / (1 + ((i + 0.5) / Precision) * ((i + 0.5) / Precision));
+		omp_set_lock(&lock);
+		pi += accumulator;
+		omp_unset_lock(&lock);
+	}
+	endTime = GetTickCount();
+	pi /= Precision;
+	std::cout <<
+		"Кол-во Потоков: " << cntThread <<
+		"\nЧисло Pi = " << fixed << setprecision(20) << pi <<
+		"\nВремя: " << endTime - startTime << " мс";
+}
+
+
